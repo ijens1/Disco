f0595af03dd1847548f3049308ebfaf893f2b8f9
Isaac Jensen-Large <jensenlarge.isaac@gmail.com>
2018-01-20 11:59:44 -0500

    Initial commit

---
+++ app/src/main/java/me/isaacdjl/disco/FeedActivity.kt
@@ -0,0 +1,12 @@
+package me.isaacdjl.disco
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class FeedActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_feed)
+    }
+}
